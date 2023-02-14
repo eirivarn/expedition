3ad12fdee1b942571c1caa5bd05c0df3ae47bd8d
Christian <chritorh@ntnu.no>
2023-02-14 12:13:39 +0100

    Satt opp Tur-side med JSX kode

--- src/pages/Trippage.jsx
+++ src/pages/Trippage.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import "../styles/Trippage.css";
 
 const TripPage = () => {
 
-    return (
-        <div>
-            <h1>Tur-siden</h1>
+  return (
+    <div className="tripPage">
+      <div className="infoTrip">
+        <h1 className="title">Trip Title</h1>
+        <h3 className="author">Ola Nordmann</h3>
+        <text className="rating">PlaceHolder Vurdering</text>
+        <img className="image"/>
+        <text className="description">Lorem Ipsum</text>
+      </div>
+      <div className="commentsTripPage">
+        <h2 className="commentSymbol">💬</h2>
+        <h2 className="commentHeader">Kommentarer </h2>
+        <div className="nameComment">
+          <label className="nameText">Navn</label>
+          <input className="nameInput"></input>
         </div>
-    ); 
+        <div className="comments">
+          <label className="commentText">Kommentar</label>
+          <input className="commentInput"></input>
+        </div>
+        <button className="publishButton">Publiser Kommentar</button>
+      </div>
+    </div>
+  );
 }
 
 export default TripPage;
