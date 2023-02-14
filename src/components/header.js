3b9817c58573c54a6778a1655e7ea6fc41df8e50
Ine Krogseth <ineak@stud.ntnu.no>
2023-02-14 11:40:11 +0100

    #1 logikk til logg inn-knapp

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import '../frontpage.css';
 import logo from '../img/xpedition_logo.png';
+import userIcon from '../img/user.png';
 
 
 export class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {isLoggedIn: false};
+    }
+    
+    handleLoginClick () {
+        this.setState({isLoggedIn: true});
+    }
+
+    handleLogoutClick () {
+        this.setState({isLoggedIn: false});
+    }
+
+
+
     render() {
+        const isLoggedIn = this.state.isLoggedIn;
+        let logButton;
+        let userButton;
+
+        if (isLoggedIn) {
+            logButton = <button id="login" className='button' type='button' onClick={this.handleLogoutClick}>Logg ut</button>
+            userButton = <button id='userButton'> <img src={userIcon} height='50px' width='50px'></img> </button>
+        }
+        else {
+            logButton = <button id='login' className='button' type='button' onClick={this.handleLoginClick}>Logg inn</button>
+            userButton = null;
+
+        }
+
+
         return (
             <div id="header">
                 <img id="logo" src={logo}></img>
-                <button id="login" className="button" type="button">Logg inn</button>
+                {userButton}
+                {logButton}
             </div>
         )
     }
