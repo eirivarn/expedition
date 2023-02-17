3e0ef57bb8d24b8801d896b6b7c7444cdb972c87
Christian <chritorh@ntnu.no>
2023-02-17 14:05:32 +0100

    Merge konflikt i TripContainer

--- src/components/TripContainer.js
+++ src/components/TripContainer.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import "../styles/Trippage.css";
-import { getDocs, collection } from "firebase/firestore";
+import { doc } from "firebase/firestore";
 import { db } from "../firebase-config.js";
 import PropTypes from "prop-types";
 
 export function TripContainer( { name }) {
   const [trip, setTrip] = useState();
 
-  const tripsCollectionRef = collection(db, "trips");
+  //const tripsCollectionRef = collection(db, "trips");
+  useEffect(() => {
+    const getTrip = async () => {
+      setTrip(doc(db, "trips", name));
+    };
+    getTrip()
+    console.log(name)
+    console.log(trip)
+  }, []);
+  /*
   useEffect(() => {
     const getTrip = async () => {
       const data = await getDocs(tripsCollectionRef);
-      setTrip(data.docs.map((doc) => ({ ...doc.data(), area: doc.area })));
-      //setTrip(trips.filter((t => t.area === id).findFirst()));
+      const trips = data.docs.map((doc) => ({ ...doc.data(), area: doc.area }));
+      setTrip(trips.filter((t => t.area === id).findFirst()));
     };
     getTrip();
     console.log(name)
-  }, []);
+  }, []); */
   
   return (
     <div>
-      <h1 className="title">{trip.map((t) => {t.name})}</h1>
+      <h1 className="title">name</h1>
       <h3 className="author">Ola Nordmann</h3>
       <img className="image" />
       <textarea className="description">trip.description</textarea>
