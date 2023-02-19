2101c3f658e6cba8febdb99a14b6e586cbc16cc3
eirivarn <eirik.varnes@gmail.com>
2023-02-19 20:35:50 +0100

    Add write to database

---
+++ src/api/api.js
@@ -0,0 +1,45 @@
+import { db } from "../firebase-config";
+import { getDocs, collection, addDoc } from "firebase/firestore";
+
+/* 
+Trips storage format
+{
+    id: string,
+    area: string,
+    countries: String[],
+    comments: String[],
+    description: string,
+    name: string,
+    rating: number
+}
+*/
+const collectionName = "trips";
+const tripsReference = collection(db, collectionName);
+
+export const getAllTrips = async () => {
+  const trips = await getDocs(tripsReference).catch((err) =>
+    console.error(err)
+  );
+  // TODO - format trips variable
+  return trips;
+};
+
+export const createTrip = async (name, countries, description) => {
+  // Add the trip data to Firestore
+  try {
+    await addDoc(tripsReference, {
+      name: name,
+      countries: countries,
+      description: description,
+    });
+  } catch (error) {
+    console.error("Error adding trip: ", error);
+  }
+};
+
+/*
+export const addComment = async (newComment) => {};
+
+// Wait to implement
+export const updateTrip = async () => {};
+*/
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,19 +1,19 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from '@firebase/firestore';
-import {getAuth,GoogleAuthProvider} from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCJlEE9yExuD5EMbXcocFqcBtHFbtSOIG0",
-    authDomain: "testprosjekt-998ef.firebaseapp.com",
-    projectId: "testprosjekt-998ef",
-    storageBucket: "testprosjekt-998ef.appspot.com",
-    messagingSenderId: "487529280599",
-    appId: "1:487529280599:web:a927b1463cabfca20e5f12",
-    measurementId: "G-SW30MN3VSY"
-  };
+  apiKey: "AIzaSyCJlEE9yExuD5EMbXcocFqcBtHFbtSOIG0",
+  authDomain: "testprosjekt-998ef.firebaseapp.com",
+  projectId: "testprosjekt-998ef",
+  storageBucket: "testprosjekt-998ef.appspot.com",
+  messagingSenderId: "487529280599",
+  appId: "1:487529280599:web:a927b1463cabfca20e5f12",
+  measurementId: "G-SW30MN3VSY",
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider(); 
+export const provider = new GoogleAuthProvider();
