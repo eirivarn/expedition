a976b3abb76d384fcf624e9ce166af2335ecb5d7
Ine Krogseth <ineak@stud.ntnu.no>
2023-03-14 14:46:09 +0100

    basic frontage toplist

---
+++ src/components/ToplistContainer.js
@@ -0,0 +1,6 @@
+export function toplistContainer() {
+
+    return (
+
+    );
+}
--- src/pages/Frontpage.jsx
+++ src/pages/Frontpage.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 //import ReactDOM from 'react-dom';
 import TripComponent from "../components/TripComponent.js";
+//import ToplistComponent from "../components/ToplistComponent.js";
 import "../styles/frontpage.css";
+import "../styles/toplist.css";
 import PropTypes from "prop-types";
 import { getAllTrips } from "../api/api.js";
 import { NavLink } from "react-router-dom";
 
+import image from "../img/test.jpg";
+
 const FrontPage = () => {
   const [trips, setTrips] = useState([]);
+  //  const [toplist, setToplist] = useState([]);
 
   /*   const getSpecificTrip = (id) => {
     const res = trips.filter((trip) => trip.id === id);
@@ -27,6 +32,7 @@ const FrontPage = () => {
       <NavLink to="/newtrip" id="shareTrip" className="button" type="button">
         Share your own adventure!
       </NavLink>
+      {/*Alle reisene på forsiden */}
       <div className="flex_images">
         <h2 className="header2">Trips</h2>
         <div className="front_grid">
@@ -50,6 +56,35 @@ const FrontPage = () => {
               </NavLink>
             );
           })}
+        </div>
+      </div>
+
+      {/*Topplisten */}
+      <div id="toplist"></div>
+      <h2>Top list</h2>
+      <div id="toplist_grid">
+        <div className="toplist_numbered">
+          <h1>1</h1>
+          <img src={image}></img>
+          <div id="toplist_name_and_rating">
+            <h3>Name trip</h3>
+            <h4>RATING</h4>
+          </div>
+        </div>
+        <div className="toplist_numbered">
+          <h1>2</h1>
+        </div>
+        <div className="toplist_numbered">
+          <h1>3</h1>
+        </div>
+        <div className="toplist_numbered">
+          <h1>4</h1>
+        </div>
+        <div className="toplist_numbered">
+          <h1>5</h1>
+        </div>
+        <div className="toplist_numbered">
+          <h1>6</h1>
         </div>
       </div>
     </div>
