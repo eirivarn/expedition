cbb3095adbe899c6d73089542059e163cfe105bc
Hafaykon <ytpmaxz@gmail.com>
2023-03-16 22:56:42 +0100

    #31 Added func for editing comment-rating.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -12,7 +12,7 @@ import {addComment, addRating} from "../api/api";
 export function Comment({ name, userId, content, rating, date, trip}) {
   const [editing, setEditing] = useState(false);
   const [text, setText] = useState(content);
-  const [ratingValue, setRating] = useState(rating)
+  const [newRating, setRating] = useState(rating)
   const isAuthor =
     auth.currentUser !== null
       ? userId === auth.currentUser.uid
@@ -23,9 +23,12 @@ export function Comment({ name, userId, content, rating, date, trip}) {
   const handleUpdateComment = async (id) => {
     if (isAuthor) {
       handleDeleteCommentButtonClick(id)
-      const commentString = userId + "::" + name + "::" + text + "::" + date + "::" + rating;
+      const commentString = userId + "::" + name + "::" + text + "::" + date + "::" + newRating;
+      console.log(newRating)
       await addComment(id, commentString)
-      console.log(commentString);
+      const newRatings = trip.rating
+      newRatings.push(newRating)
+      await addRating(id, newRatings)
     }
     handleToggle();
   };
@@ -38,9 +41,17 @@ export function Comment({ name, userId, content, rating, date, trip}) {
     if (isAuthor) {
       const commentString = userId + "::" + name + "::" + content + "::" + date + "::" + rating;
       console.log(commentString);
+      console.log(rating);
+      //trip.rating[trip.rating.indexOf(rating)] = newRating;
+      const newRatings = trip.rating
+      const i = newRatings.indexOf(rating)
+        if (i !== -1) {
+            newRatings.splice(i, 1);
+        }
+      await addRating(id, newRatings)
       const document = doc(db, "trips", id);
       await updateDoc(document, {
-        comments: arrayRemove(commentString)
+        comments: arrayRemove(commentString),
       });
     }
   };
@@ -49,13 +60,10 @@ export function Comment({ name, userId, content, rating, date, trip}) {
     <div className="commentShowBody">
       <h2 className="commentShowAuthor">{name}</h2>
       <label className="commentShowDateTime">{date}</label>
-      <Rating className="commentRating" value={trip.rating.indexOf(rating)} size="medium" readOnly={!editing} />
+      <Rating className="commentRating" value={newRating} size="medium" readOnly={!editing}
         onChange={(event, newValue) => {
           setRating(newValue);
-          trip.rating[trip.rating.indexOf(rating)] = newValue;
-          addRating(trip.id, trip.rating);
-          //calculateAverageRating();
-      }}
+      }}/>
       <textarea className="commentShowText"
       value={text}
       onChange={(event) => {
