ef99333fccad1a9cc03e7b8165202a503ca13f72
Sondre Kokkinn Rideng <sondrrid@stud.ntnu.no>
2023-03-17 13:53:13 +0100

    Fix crash when signing out

--- src/components/header.js
+++ src/components/header.js
@@ -1,54 +1,53 @@
 import React from "react";
-import {Darkmode} from './Darkmode.js';
+import { Darkmode } from "./Darkmode.js";
 import "../styles/header.css";
 import { UserAuth } from "../Context/AuthContext";
 import logo from "../img/xpedition_logo.png";
 //import "../styles/frontpage.css";
-import { NavLink} from 'react-router-dom';
-import userIcon from '../img/user.png';
+import { NavLink, useNavigate } from "react-router-dom";
+import userIcon from "../img/user.png";
 //import { useScrollDirection } from "../hooks/headerScroll";
 //import {DarkToggle} from './Darkmode.js';
 
-
-
 const Header = () => {
   const { user, logOut } = UserAuth();
+  const navigate = useNavigate();
 
   const handleSignOut = async () => {
     try {
       await logOut();
+      navigate("/");
     } catch (error) {
       console.log(error);
     }
   };
 
- // const scrollDirection = useScrollDirection();
-  
+  // const scrollDirection = useScrollDirection();
 
   return (
     <div id="header">
       <div>
-      <NavLink to="/">
-        <img id="logo" src={logo}></img>
-      </NavLink>
-      <NavLink to="/user">
-        <img id="userButton" src={userIcon}></img>
-      </NavLink>
-      {user?.displayName ? (
-        <NavLink
-          id="login"
-          className="button"
-          type="button"
-          onClick={handleSignOut}
-        >
-          Log out
+        <NavLink to="/">
+          <img id="logo" src={logo}></img>
         </NavLink>
-      ) : (
-        <NavLink id="login" className="button" type="button" to="/signin">
-          Log in
+        <NavLink to="/user">
+          <img id="userButton" src={userIcon}></img>
         </NavLink>
-      )}
-      <Darkmode></Darkmode>
+        {user?.displayName ? (
+          <NavLink
+            id="login"
+            className="button"
+            type="button"
+            onClick={handleSignOut}
+          >
+            Log out
+          </NavLink>
+        ) : (
+          <NavLink id="login" className="button" type="button" to="/signin">
+            Log in
+          </NavLink>
+        )}
+        <Darkmode></Darkmode>
       </div>
     </div>
   );
